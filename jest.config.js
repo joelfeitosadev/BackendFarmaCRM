0b0aa266b030e73114009bd00d3d68e4945ad304
Joel Feitosa da Silva <admjoelfeitosadasilva@gmail.com>
2026-05-20 21:55:28 -0300

    chore: update jest config with ts-jest diagnostics and verbose output

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,11 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/__mocks__/prisma.ts'],
+  globals: {
+    'ts-jest': {
+      diagnostics: { ignoreCodes: [151002] }
+    }
+  },
+  silent: false,
+  verbose: true,
 };
